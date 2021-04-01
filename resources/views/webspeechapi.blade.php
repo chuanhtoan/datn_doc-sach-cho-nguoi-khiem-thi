8b554a0ae8fd227d040d59e620cfdcb9fc53477b
chuanhtoan <chuanhtoan139@gmail.com>
2021-04-01 14:53:14 +0700

    speak last message

--- resources/views/webspeechapi.blade.php
+++ resources/views/webspeechapi.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Testing</title>
+
+    {{-- JQuery --}}
+    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
 </head>
 <body>
     <!-- Load Facebook SDK for JavaScript -->
