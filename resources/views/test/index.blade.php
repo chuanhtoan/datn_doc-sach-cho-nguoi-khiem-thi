6698dc9cfaba534ec00bdf99e483544b8811551f
chuanhtoan <chuanhtoan139@gmail.com>
2021-05-14 21:41:26 +0700

    testing wsa

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,136 @@
+@extends('pages.bangxephang.layout')
+
+@section('above_head')
+
+    {{-- style button thêm --}}
+    <style>
+        .btn_add {
+            float: right;
+            padding-right: 1.25rem;
+            margin-top: 1px;
+            margin-bottom: 7px;
+        }
+    </style>
+
+@endsection
+
+@section('content')
+
+<div class="mdk-header-layout__content top-navbar mdk-header-layout__content--scrollable h-100">
+    
+    <!-- main content -->
+    <div class="container-fluid">
+        
+        <h2>Bảng Xếp Hạng</h2>
+        <p class="lead">
+            Quản lý thông tin các bảng xếp hạng.
+        </p>
+        <hr>
+        <div class="card">
+            <div class="py-4">
+
+                {{-- Create Button --}}
+                <div class="btn_add">
+                    <button id="btn_add" name="btn_add" class="btn btn-success btn-detail">Add</button>
+                </div>
+
+                {{-- table --}}
+                <div class="table-responsive">
+                    <table id="data-table" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Tên Bảng Xếp Hạng</th>
+                                <th style="min-width: 110px;">Thao Tác</th>
+                            </tr>
+                        </thead>
+                        <tbody id="products-list" name="products-list">
+                            @foreach($products as $item)
+                                <tr id="product{{$item->id}}" class="active">
+                                    <td>{{$item->id}}</td>
+                                    <td>{{$item->ten}}</td>
+                                    <td>
+                                        <div style="display: inline-block">
+                                            <a href="/phim_bangxephang/{{$item->id}}" class="btn btn-primary" style="color: white">Show</a>
+                                            <button class="btn btn-warning btn-detail open_modal" value="{{$item->id}}">Edit</button>
+                                            <button class="btn btn-danger delete-product" value="{{$item->id}}">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+{{-- Create Edit Modal --}}
+<div id="createEditModal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="createEditModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="createEditModalLabel">Bảng Xếp Hạng</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="frmProducts" name="frmProducts" class="form-horizontal classFormUpdate validate-form" novalidate="">
+                    <input type="hidden" name="class_id" class="class-id" id="class-id">
+                    <div class="form-group"> {{--input-group--}}
+                        <label for="ten">Tên:</label>
+                        <input type="text" name="ten" id="ten" class="form-control required" placeholder="Tên bảng xếp hạng">
+                        <p id="textUnique" class="invalid-feedback d-inline text-danger"></p>
+                        <input type="hidden" id="idUser" name="idUser" value="{{$nguoidung->id}}">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" id="btn-cancel" data-dismiss="modal">Hủy</button>
+                <button type="button" class="btn btn-primary" id="btn-save" value="update">Lưu</button>
+                <input type="hidden" id="product_id" name="product_id" value="0">
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- Delete Modal --}}
+<div id="deleteModal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="deleteModalLabel">Thông báo!</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <label id="lableXoa"></label>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Hủy</button>
+                <button type="button" class="btn btn-primary" id="btn-delete" value="delete">Xoá</button>
+                <input type="hidden" id="product_id_delete" name="product_id_delete" value="0">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+@section('above_body')
+
+    {{-- validate --}}
+    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+
+    {{-- ajax thêm xóa sửa --}}
+    @include('pages.bangxephang.ajaxscript')
+    @yield('ajax')
+
+    {{-- alertify --}}
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+@endsection
+
---
+++ resources/views/test/layout.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Utako | Admin Page</title>
+
+
+    <link type="text/css" href="{{asset('backend/css/vendor-morris.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('backend/css/vendor-bootstrap-datepicker.css')}}" rel="stylesheet">
+
+    <!-- Prevent the demo from appearing in search engines -->
+    <meta name="robots" content="noindex">
+
+    <!-- App CSS -->
+    <link type="text/css" href="{{asset('backend/css/app.css')}}" rel="stylesheet">
+    <link type="text/css" href="{{asset('backend/css/app.rtl.css')}}" rel="stylesheet">
+
+    <!-- Simplebar -->
+    <link type="text/css" href="{{asset('backend/vendor/simplebar.css')}}" rel="stylesheet">
+
+    {{-- Datatable --}}
+    <link type="text/css" href="{{asset('backend/css/vendor-bootstrap-datatables.css')}}" rel="stylesheet">
+    {{-- /Datatable --}}
+
+    {{-- Ajax Popup --}}
+    <!-- Important to work AJAX CSRF -->
+    <meta name="_token" content="{!! csrf_token() !!}" />
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    {{-- /Ajax Popup --}}
+
+    {{-- alertify --}}
+    <!-- CSS -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
+    <!-- Default theme -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css" />
+    <!-- Semantic UI theme -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/semantic.min.css" />
+    {{-- /alertify --}}
+
+    {{-- web icon --}}
+    <link rel="shortcut icon" href="{{asset('backend/images/icon.png')}}" />
+
+    {{-- above_head --}}
+    @yield('above_head')
+
+</head>
+
+<body>
+    <div class="mdk-drawer-layout js-mdk-drawer-layout" data-fullbleed data-push data-responsive-width="992px" data-has-scrolling-region>
+
+        <div class="mdk-drawer-layout__content">
+            <!-- header-layout -->
+            <div class="mdk-header-layout js-mdk-header-layout  mdk-header--fixed  mdk-header-layout__content--scrollable">
+                
+                {{-- <!-- begin header -->
+                    @include('admin.layout.header')
+                    @yield('header')
+                <!-- end header --> --}}
+
+                <!-- begin content -->
+                    @include('admin.layout.content')
+                    @yield('content')
+                <!-- end content -->
+
+            </div>
+
+        </div>
+        {{-- <!-- // END drawer-layout__content -->
+            @include('admin.layout.left_drawer')
+            @yield('left_drawer')
+        <!-- drawer -->
+        
+        <!-- // END drawer -->
+
+        <!-- drawer -->
+            @include('admin.layout.right_drawer')
+            @yield('right_drawer')
+        <!-- // END drawer --> --}}
+
+    </div>
+    <!-- // END drawer-layout -->
+
+
+
+    <!-- jQuery -->
+    {{-- <script src="{{asset('backend/vendor/jquery.min.js')}}"></script> --}}
+
+    <!-- Bootstrap -->
+    <script src="{{asset('backend/vendor/popper.js')}}"></script>
+    <script src="{{asset('backend/vendor/bootstrap.min.js')}}"></script>
+
+    <!-- Simplebar -->
+    <!-- Used for adding a custom scrollbar to the drawer -->
+    <script src="{{asset('backend/vendor/simplebar.js')}}"></script>
+
+
+    <!-- Vendor -->
+    <script src="{{asset('backend/vendor/Chart.min.js')}}"></script>
+    <script src="{{asset('backend/vendor/moment.min.js')}}"></script>
+
+    <!-- APP -->
+    <script src="{{asset('backend/js/color_variables.js')}}"></script>
+    <script src="{{asset('backend/js/app.js')}}"></script>
+
+
+    <script src="{{asset('backend/vendor/dom-factory.js')}}"></script>
+    <!-- DOM Factory -->
+    <script src="{{asset('backend/vendor/material-design-kit.js')}}"></script>
+    <!-- MDK -->
+
+
+
+    <script>
+        (function() {
+            'use strict';
+            // Self Initialize DOM Factory Components
+            domFactory.handler.autoInit()
+
+
+            // Connect button(s) to drawer(s)
+            var sidebarToggle = document.querySelectorAll('[data-toggle="sidebar"]')
+
+            sidebarToggle.forEach(function(toggle) {
+                toggle.addEventListener('click', function(e) {
+                    var selector = e.currentTarget.getAttribute('data-target') || '#default-drawer'
+                    var drawer = document.querySelector(selector)
+                    if (drawer) {
+                        if (selector == '#default-drawer') {
+                            $('.container-fluid').toggleClass('container--max');
+                        }
+                        drawer.mdkDrawer.toggle();
+                    }
+                })
+            })
+        })()
+    </script>
+
+
+    <script src="{{asset('backend/vendor/morris.min.js')}}"></script>
+    <script src="{{asset('backend/vendor/raphael.min.js')}}"></script>
+    <script src="{{asset('backend/vendor/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{asset('backend/js/datepicker.js')}}"></script>
+
+    {{-- Datatable --}}
+    <script src="{{asset('backend/vendor/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('backend/vendor/dataTables.bootstrap4.js')}}"></script>
+
+    <script>
+        $('#data-table').dataTable();
+    </script>
+    {{-- /Datatable --}}
+
+
+
+    @yield('above_body')
+
+</body>
+
+
+</html>
