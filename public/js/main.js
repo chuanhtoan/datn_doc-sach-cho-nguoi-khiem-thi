9225f37e7f2a2772845ecbb879271cba137ec7af
chuanhtoan139 <chuanhtoan139@gmail.com>
2021-06-27 16:35:08 +0700

    database config

--- public/js/main.js
+++ public/js/main.js
@@ -383,15 +383,7 @@ function checking(mess) {
         command.startsWith("phim ") ||
         command.startsWith("xem ")
     ) {
-        let chapter = toNum(command.split(" ").pop());
-        if (
-            chapter > 0 &&
-            chapter <= document.querySelectorAll(".chapters__list--item").length
-        ) {
-            window.location.href = window.location.pathname + "/" + chapter;
-        } else {
-            responsiveVoice.speak("Chương không tồn tại");
-        }
+        // test
     } else if (command == "doc") {
         responsiveVoice.speak(
             document.querySelector("#novel-content").innerText
